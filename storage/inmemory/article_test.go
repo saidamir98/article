14af2d9136ec30f05b7311ff6ec05c16438571f5
Saidamir Botirov <saidamir.botirov@gmail.com>
2022-10-11 19:31:40 +0500

    TestGetArticleByID

--- storage/inmemory/article_test.go
+++ storage/inmemory/article_test.go
@@ -7,14 +7,6 @@ import (
 	"uacademy/article/storage/inmemory"
 )
 
-type AddArticleTestModel struct {
-	name       string
-	id         string
-	data       models.CreateArticleModel
-	wantError  error
-	wantResult models.PackedArticleModel
-}
-
 func TestAddArticle(t *testing.T) {
 	var err error
 	IM := inmemory.InMemory{
@@ -39,7 +31,13 @@ func TestAddArticle(t *testing.T) {
 		t.Fatalf("unexpected err: %v", err)
 	}
 
-	var tests []AddArticleTestModel = []AddArticleTestModel{
+	var tests = []struct {
+		name       string
+		id         string
+		data       models.CreateArticleModel
+		wantError  error
+		wantResult models.PackedArticleModel
+	}{
 		{
 			name: "success",
 			id:   "20455551-7263-4009-91bd-3fa6a10e3827",
@@ -84,6 +82,87 @@ func TestAddArticle(t *testing.T) {
 			} else {
 				if tt.wantError.Error() != err.Error() {
 					t.Errorf("IM.AddArticle() expected error: %v, but got error: %v", tt.wantError, err)
+				}
+			}
+		})
+	}
+	t.Log("Test has been finished")
+}
+
+func TestGetArticleByID(t *testing.T) {
+	var err error
+	IM := inmemory.InMemory{
+		Db: &inmemory.DB{},
+	}
+
+	errorArticleNotFound := errors.New("article not found")
+
+	authorID := "626f1e10-58a2-414e-83c5-899b92ea0ff5"
+	authorData := models.CreateAuthorModel{
+		Firstname: "John",
+		Lastname:  "Doe",
+	}
+	err = IM.AddAuthor(authorID, authorData)
+	if err != nil {
+		t.Fatalf("unexpected err: %v", err)
+	}
+
+	articleID := "20455551-7263-4009-91bd-3fa6a10e3827"
+	articleData := models.CreateArticleModel{
+		Content: models.Content{
+			Title: "Lorem",
+			Body:  "Impsume",
+		},
+		AuthorID: authorID,
+	}
+	err = IM.AddArticle(articleID, articleData)
+	if err != nil {
+		t.Fatalf("unexpected err: %v", err)
+	}
+
+	var tests = []struct {
+		name       string
+		id         string
+		mockFunc   func()
+		wantError  error
+		wantResult models.PackedArticleModel
+	}{
+		{
+			name: "success",
+			id:   articleID,
+			mockFunc: func() {
+
+			},
+			wantError: nil,
+			wantResult: models.PackedArticleModel{
+				Content: articleData.Content,
+			},
+		},
+		{
+			name: "fail: artilce not found",
+			id:   "30455551-7263-4009-91bd-3fa6a10e3827",
+			mockFunc: func() {
+
+			},
+			wantError:  errorArticleNotFound,
+			wantResult: models.PackedArticleModel{},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			article, err := IM.GetArticleByID(tt.id)
+			if tt.wantError == nil {
+				if err != nil {
+					t.Errorf("IM.GetArticleByID() got error: %v", err)
+				}
+
+				if tt.wantResult.Content != article.Content {
+					t.Errorf("IM.GetArticleByID() expected: %v but got: %v", tt.wantResult.Content, article.Content)
+				}
+			} else {
+				if tt.wantError.Error() != err.Error() {
+					t.Errorf("IM.GetArticleByID() expected error: %v, but got error: %v", tt.wantError, err)
 				}
 			}
 		})
